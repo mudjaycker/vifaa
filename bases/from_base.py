97830c4a189595c26f10000691bcc4076407e4d1
mudjaycker <tombutoyi@gmail.com>
2023-09-10 21:40:59 +0200

    from_base function

--- bases/from_base.py
+++ bases/from_base.py
@@ -1,13 +1,15 @@
 from const import numbers_map
 
-numbers_map = {v: k for k, v in numbers_map.items()}
+map = {v: k for k, v in numbers_map.items()}
 
-def from_base(number:str, base:int=2) -> int:
+
+def from_base(number: str, base: int = 2) -> int:
     result = 0
     number = reversed([i for i in number])
-    for i,v in enumerate(number):
-        char = int(numbers_map[v])
-        result += (char*(base**i))
-    print(result)
+    for i, v in enumerate(number):
+        char = int(map[v])
+        result += char * (base**i)
+    return result
 
-from_base("1B", 16)
+
+# print(from_base("1B", 16)) # /=> 27
