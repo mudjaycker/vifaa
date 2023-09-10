2bb64b6dab62bb3441628f2d72fe2ecd88e747fe
mudjaycker <tombutoyi@gmail.com>
2023-09-10 20:43:53 +0200

    optimize to_base function

--- utils/to_base.py
+++ utils/to_base.py
@@ -5,19 +5,18 @@ def to_base(number: int, base: int = 2) -> str:
     temp = number
     numbers_map = {i: str(i) for i in range(10)} \
                    | {k + 10: v for k, v in enumerate(ascii_uppercase)}
-
+    result = ""
     if number == 0:
         return "0"
     while temp > 0:
         mod = temp % base
         temp //= base
         mod = numbers_map[mod]
-        tab.append(mod)
+        result = mod + result
 
-    result = "".join(reversed(tab))
     return result
 
 
 number = 27
-print(to_base(number, 5))  # /=> 1B
+print(to_base(number, 16))  # /=> 1B
 print(to_base(number, 2))  # /=> 11011
