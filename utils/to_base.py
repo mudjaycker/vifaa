f35cee0e7b9d4872256b3e3474da8c51cbd0253a
mudjaycker <tombutoyi@gmail.com>
2023-09-10 20:41:23 +0200

    use "|" operator

--- utils/to_base.py
+++ utils/to_base.py
@@ -1,24 +1,23 @@
 from string import ascii_uppercase
 
-
 def to_base(number: int, base: int = 2) -> str:
     tab = []
     temp = number
-    _ = {k + 10: v for k, v in enumerate(ascii_uppercase)}
-    hexa_map = {i: str(i) for i in range(1, 10)}
-    hexa_map.update(_)
+    numbers_map = {i: str(i) for i in range(10)} \
+                   | {k + 10: v for k, v in enumerate(ascii_uppercase)}
+
     if number == 0:
         return "0"
     while temp > 0:
         mod = temp % base
         temp //= base
-        mod = hexa_map[mod] if base > 15 else str(mod)
+        mod = numbers_map[mod]
         tab.append(mod)
 
     result = "".join(reversed(tab))
     return result
 
 
-number = 279
-print(to_base(number, 27))  # /=> 1B
+number = 27
+print(to_base(number, 5))  # /=> 1B
 print(to_base(number, 2))  # /=> 11011
