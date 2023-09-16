752735ff48b84ccf903e2fc533233570d59f0839
mudjaycker <tombutoyi@gmail.com>
2023-09-16 22:50:11 +0200

    rename the imporeted const.number_map to inverted_map for more readabilty

--- bases/from_base.py
+++ bases/from_base.py
@@ -1,13 +1,13 @@
 from const import numbers_map
 
-map = {v: k for k, v in numbers_map.items()}
+inverted_map = {v: k for k, v in numbers_map.items()}
 
 
 def from_base(number: str, base: int = 2) -> int:
     result = 0
     number = reversed([i for i in number])
     for i, v in enumerate(number):
-        char = int(map[v])
+        char = int(inverted_map[v])
         result += char * (base**i)
     return result
 
