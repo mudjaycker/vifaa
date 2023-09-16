f83d80a8c4f3fcf32966e6c56354037c4e84fca1
mudjaycker <tombutoyi@gmail.com>
2023-09-16 22:48:09 +0200

    make const.numbers_map more readble

--- bases/const.py
+++ bases/const.py
@@ -1,3 +1,5 @@
 from string import ascii_uppercase
-numbers_map = {i: str(i) for i in range(10)} \
-                   | {k + 10: v for k, v in enumerate(ascii_uppercase)}
+numbers_map = (
+                {i: str(i) for i in range(10)} |
+                {k + 10: v for k, v in enumerate(ascii_uppercase)}
+            )
