3015efe46ec529613292ff5b7e829cfc57edeef2
mudjaycker <tombutoyi@gmail.com>
2024-04-04 21:38:52 +0200

    add modulo 24 to hours

--- media/media_duration.py
+++ media/media_duration.py
@@ -6,32 +6,34 @@ import time
 
 class MDuration:
     def __init__(self, path:str="."):
+        self._extensions = ["mkv", "mp4", "m4a", "avi", "mpeg", "mp3", "webm"]
         self.founds = self.getFiles(path)
         
     def length(self, filename):
-        result = subprocess.run(
-            [
-                "ffprobe",
-                "-v",
-                "error",
-                "-show_entries",
-                "format=duration",
-                "-of",
-                "default=noprint_wrappers=1:nokey=1",
-                filename,
-            ],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-        )
-        return float(result.stdout)
+        try:
+            result = subprocess.run(
+                [
+                    "ffprobe",
+                    "-v",
+                    "error",
+                    "-show_entries",
+                    "format=duration",
+                    "-of",
+                    "default=noprint_wrappers=1:nokey=1",
+                    filename,
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+            )
+            return float(result.stdout)
+        except: return 0.0
 
 
     def getFiles(self, path: str):
         p = Path(path).resolve()
-        extensions = ["mkv", "mp4", "m4a", "avi", "mpeg", "mp3", "webm"]
         find = lambda x: list(p.glob(f"**/*.{x}"))
         founds = []
-        for f in extensions:
+        for f in self._extensions:
             founds += find(f)
         return founds
     
@@ -42,18 +44,18 @@ class MDuration:
             res = th.map(self.length, self.founds)
         seconds = sum(res)
         durations = [
-            int(seconds // 3600), #hours
+            int(seconds // 3600)%24, #hours
             int((seconds / 60) % 60), #minutes
             int(seconds % 60 )#seconds
         ]
         return seconds, durations
     
 
-# if __name__ == '__main__':
-    # b = time.perf_counter()
-    # path = "/media/maryimana/DISK 2/Schoolside/C/"
-    # x = MDuration(path)
-    # durations = x.get_durations()
-    # print(durations[1])
-    # e = time.perf_counter()
-    # print(f"took: {e-b} seconds")
+if __name__ == '__main__':
+    b = time.perf_counter()
+    path = "/home/maryimana/"
+    x = MDuration(path)
+    durations = x.get_durations()
+    print(durations[1])
+    e = time.perf_counter()
+    print(f"took: {e-b} seconds")
