801e0ffe6125d2e68c74272cb3d63f3edff4f3f5
mudjaycker <tombutoyi@gmail.com>
2024-10-02 20:17:19 +0200

    ts from base

---
+++ typescript/bases/from_base.ts
@@ -0,0 +1,24 @@
+import NUM_MAP from "./consts";
+
+const INVERTED_MAP = new Map();
+
+for (let data of NUM_MAP) {
+  INVERTED_MAP.set(data[1], data[0]);
+}
+
+function from_base(number_: string, base: number = 2): number {
+  let result = 0;
+  let number_list = number_
+    .split("")
+    .map((x) => x.toUpperCase())
+    .reverse();
+
+  for (let i = 0; i < number_list.length; i++) {
+    let v = number_list[i];
+    let char = INVERTED_MAP.get(v);
+
+    result += char * Math.pow(base, i);
+  }
+
+  return result;
+}
