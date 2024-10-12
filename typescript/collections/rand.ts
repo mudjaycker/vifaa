5e44f1b6488e2b579e84b0299aa7c6731b3c4456
mudjaycker <tombutoyi@gmail.com>
2024-10-12 23:49:20 +0200

    init rand.ts

---
+++ typescript/collections/rand.ts
@@ -0,0 +1,23 @@
+import { range } from "./collections";
+
+function randint(from_: number = 0, to: number = 0) {
+  if (to == 0) {
+    [from_, to] = [to, from_];
+  }
+  let multiple = to > 100 ? to : 100;
+  let rand = parseInt(`${Math.random() * multiple}`, 10);
+  let mod = rand % (to + 1);
+  let result = mod;
+  if (result < from_) {
+    for (let i of range(to)) {
+      result += i;
+      if (result > from_) {
+        if (result >= to) {
+          result = to;
+        }
+        break;
+      }
+    }
+  }
+  return result;
+}
