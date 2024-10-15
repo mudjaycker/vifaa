9b057b8b9ba622fb137bb700ee44e0dfa01ba58a
mudjaycker <tombutoyi@gmail.com>
2024-10-15 20:55:05 +0200

    update rand.ts(make randint function more performante)

--- typescript/collections/rand.ts
+++ typescript/collections/rand.ts
@@ -4,11 +4,11 @@ function randint(from_: number = 0, to: number = 0) {
   if (to == 0) {
     [from_, to] = [to, from_];
   }
-  let array = list(range(from_, to + 1));
+  let ecart = to - from_;
   let multiple = to > 100 ? to : 100;
   let rand = Math.round(Math.random() * multiple);
-  let index = rand % (array.length - 1);
-  return array[index];
+  let modulo = rand % ecart;
+  return from_ + modulo;
 }
 
 function choice(iterable: Iterable<any>) {
@@ -26,12 +26,3 @@ function randrange(
   let range_result = range(from_, to, step);
   return choice(range_result);
 }
-
-let x: number[] = [];
-
-for (let i of range(1000)) {
-  x.push(randint(0, 1000));
-}
-let y = uniquify(x);
-print(x.length - y.length);
-print(x.length, y.length);
