f0171c9150813d2241fb8cfbb4e6935bd48450f3
mudjaycker <tombutoyi@gmail.com>
2024-10-15 20:41:38 +0200

    update rand.ts (remove unperformant randint function)

--- typescript/collections/rand.ts
+++ typescript/collections/rand.ts
@@ -1,43 +1,20 @@
 import { range, list, print, uniquify } from "./collections";
 
-function randint(from_: number = 0, to: number = 0): number {
+function randint(from_: number = 0, to: number = 0) {
   if (to == 0) {
     [from_, to] = [to, from_];
   }
-  let multiple = to > 10 ? to : 10;
+  let array = list(range(from_, to + 1));
+  let multiple = to > 100 ? to : 100;
   let rand = Math.round(Math.random() * multiple);
-  let result = rand % (to + 1);
-  let array = list(range(from_, to + 1));
-
-  if (result < from_) {
-    for (let i of range(to + 1)) {
-      result += i;
-      if (result > from_) {
-        if (result > to) {
-          let tempo = i % (array.length - 1);
-          result = array[tempo];
-        }
-        break;
-      }
-    }
-  }
-  return result;
-}
-
-function randint2(from_: number = 0, to: number = 0) {
-  if (to == 0) {
-    [from_, to] = [to, from_];
-  }
-  let array = list(range(from_, to + 1));
-  let rand = Math.round(Math.random() * 100);
-  let index = rand % array.length;
+  let index = rand % (array.length - 1);
   return array[index];
 }
 
 function choice(iterable: Iterable<any>) {
   let array = list(iterable);
   let last_index = array.length - 1;
-  let randIndex = randint2(last_index);
+  let randIndex = randint(last_index);
   return array[randIndex];
 }
 
@@ -50,11 +27,11 @@ function randrange(
   return choice(range_result);
 }
 
-// let x: number[] = [];
+let x: number[] = [];
 
-// for (let i of range(100)) {
-// x.push(randint2(0, 100));
-// }
-// let y = uniquify(x);
-// print(x.length - y.length);
-// print(x.length, y.length);
+for (let i of range(1000)) {
+  x.push(randint(0, 1000));
+}
+let y = uniquify(x);
+print(x.length - y.length);
+print(x.length, y.length);
