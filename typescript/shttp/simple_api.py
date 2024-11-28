6c4fcfe64c3b9e74a115e60a8b8200155219a5f6
mudjaycker <tombutoyi@gmail.com>
2024-11-28 16:41:00 +0200

    serve DELETE method

--- typescript/shttp/simple_api.py
+++ typescript/shttp/simple_api.py
@@ -1,39 +1,38 @@
-#Fixing Flask dependency error which happens when python version is  >= 3.10
 import collections
 import sys
+
 if sys.version_info.major == 3 and sys.version_info.minor >= 10:
     from collections.abc import MutableSet
 
     collections.MutableSet = collections.abc.MutableSet
 else:
     from collections import MutableSet
-
-
 # creating a Flask app
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)#Fixing cors browser error
+CORS(app)
 
+
+# on the terminal type: curl http://127.0.0.1:5000/
+# returns hello world when we use GET.
+# returns the data that we send when we use POST.
 @app.route("/", methods=["GET", "POST", "PUT", "DELETE"])
 def home():
     if request.method == "GET":
 
         data = "hello world"
         return jsonify({"data": data})
-    
     elif request.method == "POST":
         data = request.get_json()
         return jsonify({"data": data}), 201
-    
     elif request.method == "PUT":
         data = request.get_json()
         return jsonify({"data": data}), 200
     
     elif request.method == "DELETE":
-        data = request.get_json()
-        return jsonify({"data": data}), 204
+        return jsonify({"message": "Data deleted"}), 204
 
 
 if __name__ == "__main__":
