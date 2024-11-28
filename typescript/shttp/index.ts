942157a3acfc59b5bc9d96023c95aa65be4761a3
mudjaycker <tombutoyi@gmail.com>
2024-11-28 12:51:56 +0200

    init and test put method

--- typescript/shttp/index.ts
+++ typescript/shttp/index.ts
@@ -32,6 +32,23 @@ class Shttp {
       REQUEST.send(JSON.stringify(data));
     });
   }
+
+  put(url: string, data: object, header = {}) {
+    const REQUEST = new XMLHttpRequest();
+    REQUEST.open("PUT", url);
+    REQUEST.setRequestHeader("Accept", "application/json");
+    REQUEST.setRequestHeader("Content-Type", "application/json");
+    return new Promise((resolve, reject) => {
+      REQUEST.onload = () => {
+        if (REQUEST.status >= 200 && REQUEST.status < 300) {
+          resolve(JSON.parse(REQUEST.response));
+        } else {
+          reject(REQUEST);
+        }
+      };
+      REQUEST.send(JSON.stringify(data));
+    });
+  }
 }
 /*  
 ?Test the get method
@@ -51,3 +68,14 @@ new Shttp()
   .catch((e) => {
     console.log("===>", e);
   }); */
+
+/*
+  ?Test the put method 
+  new Shttp()
+.put("http://127.0.0.1:5000/", { data: "John Doe Updated" })
+.then((e) => {
+    console.log(e);
+})
+.catch((e) => {
+    console.log("===>", e);
+}); */
--- typescript/shttp/test/index.js
+++ typescript/shttp/test/index.js
@@ -32,8 +32,24 @@ class Shttp {
       REQUEST.send(JSON.stringify(data));
     });
   }
+  put(url, data, header = {}) {
+    const REQUEST = new XMLHttpRequest;
+    REQUEST.open("PUT", url);
+    REQUEST.setRequestHeader("Accept", "application/json");
+    REQUEST.setRequestHeader("Content-Type", "application/json");
+    return new Promise((resolve, reject) => {
+      REQUEST.onload = () => {
+        if (REQUEST.status >= 200 && REQUEST.status < 300) {
+          resolve(JSON.parse(REQUEST.response));
+        } else {
+          reject(REQUEST);
+        }
+      };
+      REQUEST.send(JSON.stringify(data));
+    });
+  }
 }
-new Shttp().post("http://127.0.0.1:5000/", { data: "John Doe" }).then((e) => {
+new Shttp().put("http://127.0.0.1:5000/", { data: "John Doe Updated" }).then((e) => {
   console.log(e);
 }).catch((e) => {
   console.log("===>", e);
