4e5e553a25c159c598f7b59e32d2ccd99620bd67
mudjaycker <tombutoyi@gmail.com>
2024-11-28 17:05:20 +0200

    replace remove spaces in the string number parameter

--- bases/from_base.py
+++ bases/from_base.py
@@ -5,6 +5,7 @@ inverted_map = {v: k for k, v in numbers_map.items()}
 
 def from_base(number: str, base: int = 2) -> int:
     result = 0
+    number = number.replace(" ", "")
     number = reversed([i for i in number.upper()])
     for i, v in enumerate(number):
         char = int(inverted_map[v])
@@ -12,4 +13,4 @@ def from_base(number: str, base: int = 2) -> int:
     return result
 
 
-# print(from_base("1B", 16)) # /=> 27
+# print(from_base("309b 66f6 88bf ad72", 16)) # /=> 3502506344252943730
