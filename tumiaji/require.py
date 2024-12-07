69299bce68371994f4f9ff52cca131744a49591f
mudjaycker <tombutoyi@gmail.com>
2024-12-07 23:00:34 +0200

    update tumiaji.require

--- tumiaji/require.py
+++ tumiaji/require.py
@@ -33,7 +33,7 @@ class Require:
         str_file_path = str_file_path + '.py' if file_path.suffix != '.py' else str_file_path
         file_path = Path(str_file_path)
 
-        if not file_path.is_file() and not file_path.exists():
+        if not file_path.is_file() or not file_path.exists():
             raise FileNotFoundError(f"File not found: {file_path}")
         
         
@@ -42,8 +42,8 @@ class Require:
         module = importlib.util.module_from_spec(spec_loc)
         sys.modules[module_name] = module
         spec_loc.loader.exec_module(module)
-        # imported: ModuleType = __import__(module_name, globals(), locals(), ["*"], 0) #not advised by python documentation
-        imported: ModuleType = import_module(module_name)
+        imported: ModuleType = __import__(module_name, globals(), locals(), ["*"], 0) #not advised by python documentation
+        # imported: ModuleType = import_module(module_name)
         del sys.modules[module_name]
         return imported
 
