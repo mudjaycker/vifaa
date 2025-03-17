bccd3ed8fd4f1bfc5d04189d5bdbfd13f752b74d
mudjaycker <tombutoyi@gmail.com>
2025-03-17 21:19:54 +0200

    py: Get weekday of any date tool

---
+++ tumiaji/week_day.py
@@ -0,0 +1,22 @@
+from datetime import date as dt
+import typing as t
+
+
+def get_weekday(date_: t.Iterable[int]):
+    today, td_weekday = dt.today(), dt.today().weekday()
+    target_date = dt(*date_)
+    days = abs((today - target_date).days) #always get positif result
+    iterations = days % 7
+    days_map = list(range(7))  # range from 0 to 6
+    days_map = list(reversed(days_map)) if target_date < today else days_map
+    result = (
+        days_map[iterations] + td_weekday + 1
+        if target_date < today
+        else days_map[iterations] + td_weekday
+    ) % 7
+    return result
+
+
+list_date = [1969, 9, 28]
+calcule = get_weekday(list_date)
+print(calcule, dt(*list_date).weekday())
