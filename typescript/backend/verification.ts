c3186b4967cc6dcb28fd835b3cb50d9d45a565a4
mudjaycker <tombutoyi@gmail.com>
2025-03-31 00:29:01 +0200

    ts: update verifications

---
+++ typescript/backend/verification.ts
@@ -0,0 +1,36 @@
+class IterableToCheck<T> {
+    iterable: string | T[];
+
+    constructor(iterable: string | T[]) {
+        this.iterable = iterable;
+    }
+
+    isEmpty() {
+        return this.iterable.length == 0 || this.hasOnly(" ");
+    }
+    hasOnly(value: T | string) {
+        let array = Object.values(this.iterable);
+        const result = array.filter((x) => x != value);
+        return !result.length;
+    }
+
+    hasRepeated(value: T | string): { result: boolean; count: number } {
+        let array = Object.values(this.iterable);
+        let count = 0;
+
+        if (array.includes(value)) {
+            for (let i of array) {
+                if (i == value) {
+                    count++;
+                }
+            }
+        }
+        return { result: count > 1, count };
+    }
+}
+
+const check = <T>(iterable: T[] | string) => new IterableToCheck(iterable); // shortcut do that we don't need to instance each time
+
+const x = "allemagne nazi";
+const c = check(x);
+console.log(c.hasRepeated(5));
