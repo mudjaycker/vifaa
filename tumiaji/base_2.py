a336dca514d6137ef66a66e23b5163ce5f0fb8a8
mudjaycker <tombutoyi@gmail.com>
2025-03-30 23:46:28 +0200

    py: init base_2

---
+++ tumiaji/base_2.py
@@ -0,0 +1,16 @@
+def base_2(number: int):
+    results = []
+    j = 0
+    for i in range(number, -1, -1):
+        j += 2**i
+        if j <= number:
+            results.append(i)
+        else:
+            j -= 2**i
+    return results
+
+
+# number = 33
+# res = base_2(number)
+# test = sum([2**i for i in res])
+# print(f"{res=}\n{test=}")
