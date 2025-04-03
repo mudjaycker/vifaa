d0c712fcf456384cd6f63b69528698155fb8a55a
mudjaycker <tombutoyi@gmail.com>
2025-04-03 21:48:23 +0200

    update aduration

--- tumiaji/aduration.py
+++ tumiaji/aduration.py
@@ -1,16 +1,17 @@
-import asyncio
-import datetime as dt
-import time
+import datetime as dt, typing as t, asyncio
 
 sleep = asyncio.sleep
-
+T = t.TypeVar("T")
+T_Args = t.ParamSpecArgs
+T_Func = t.Callable[[T_Args], t.Coroutine[t.Any, None, T]] | t.Callable[[], t.Coroutine[t.Any, None, T]]
 
 class aWaitFor:
-    def __init__(self, seconds: int = 0):
+    def __init__(self, seconds: float = 0):
         self.seconds = seconds
 
-    def __call__(self, function):
-        async def wrapper(*args, **kwargs):
+    def __call__[T](self, function: T_Func[T]):
+        
+        async def wrapper(*args:T_Args, **kwargs: T_Args):
             await sleep(self.seconds)
             return await function(*args, **kwargs)
 
@@ -18,11 +19,12 @@ class aWaitFor:
 
 
 class aInterval:
-    def __init__(self, seconds: int = 0):
+    def __init__(self, seconds: float = 0):
         self.seconds = seconds
 
-    def __call__(self, function):
-        async def wrapper(*args, **kwargs):
+    def __call__[T](self, function: T_Func[T]):
+        
+        async def wrapper(*args:T_Args, **kwargs:T_Args):
             while self.seconds:
                 await function(*args, **kwargs)
                 await sleep(self.seconds)
@@ -31,50 +33,53 @@ class aInterval:
 
 
 class aRunTill:
-    def __init__(self, seconds: int = 0, timeout: int = 0):
+    def __init__(self, seconds: float = 0, timeout: float = 0):
         self.seconds = seconds
         self.timeout = timeout
         # self.datas = []
 
-    def __call__(self, function):
-
-        async def wrapper(*args, **kwargs):
+    def __call__[T](self, function: T_Func[T]):
+        
+        async def wrapper(*args:T_Args, **kwargs:T_Args):
             stamp = lambda: (dt.datetime.timestamp(dt.datetime.now()))
             target_time = self.timeout + stamp()
-
+            res = None
             while self.seconds:
                 if stamp() >= target_time:
-                    return
-                await function(*args, **kwargs)
+                    return res
+                res = await function(*args, **kwargs)
                 await sleep(self.seconds)
 
         return wrapper
 
 
-i = 0
+""" i = 0
 j = 0
 
 
-@aRunTill(0.2, 5)
+@aRunTill(1, 3)
 async def test():
     global i
     i += 1
     print(i, "a")
+    return i
 
 
-@aRunTill(3, 2)
+@aRunTill(0.6, 4)
 async def test2():
-    global j
-    j += 1
+    global j, i
     print(j, "b")
+    if i >= 10:
+        print(i >= 10)
+    j += 1
 
 
 async def main():
     await asyncio.gather(
         test2(),
+        test(),
     )
 
 
-b = time.time()
 asyncio.run(main())
-print(time.time() - b)
+print(f"{i=}, {j=}") """
